e2a5dfa772aedbcff16f9a6398b7dace29248991
vip-dacha86 <vip-dacha86@yandex.ru>
2023-02-14 10:25:19 +0500

    Example006_Маша/

---
+++ 006/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("введите имя пользователя: ");
+string username = Console.ReadLine("");
+
+if(username == "Маша");
+{
+       Console.WriteLine("Ура, это же МАША!");
+}
+else
+{
+       Console.Write("Привет, ");
+       Console.WriteLine(username); 
+}
