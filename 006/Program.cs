dbda41149da0ab9bda75fd4c5c1598539d34537c
vip-dacha86 <vip-dacha86@yandex.ru>
2023-02-14 12:14:39 +0500

    Example008_Magic/

--- 006/Program.cs
+++ 006/Program.cs
@@ -1,12 +1,13 @@
 ﻿Console.Write("введите имя пользователя: ");
-string username = Console.ReadLine("");
+string username = Console.ReadLine();
 
-if(username == "Маша");
+if (username == "Маша") 
 {
-       Console.WriteLine("Ура, это же МАША!");
+Console.WriteLine("Ура, это же МАША!");
 }
 else
 {
-       Console.Write("Привет, ");
-       Console.WriteLine(username); 
+ Console.Write("Привет, ");
+Console.WriteLine(username);
 }
+    
