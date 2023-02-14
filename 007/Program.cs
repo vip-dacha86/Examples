a536f7d358767c6cf65fc41cde79da2d2e3df9cf
vip-dacha86 <vip-dacha86@yandex.ru>
2023-02-14 10:38:41 +0500

    Example007_Base/

---
+++ 007/Program.cs
@@ -0,0 +1,16 @@
+﻿int a=21;
+int b=2;
+int c=68;
+int d=82;
+int e=4;
+
+int max=a;
+
+if(a>max)max=a;
+if(b>max)max=b;
+if(c>max)max=c;
+if(d>max)max=d;
+if(e>max)max=e;
+
+Console.Write("max=");
+Console.WriteLine(max);
